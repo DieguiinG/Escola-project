8de828392d6a1b773b118dd55b85c654d1b94c2f
DieguiinG <difejacyntho@hotmail.com>
2023-09-29 19:55:45 -0300

    StudentModel criado

---
+++ src/main/java/com/example/Escola/models/StudentModel.java
@@ -0,0 +1,71 @@
+package com.example.Escola.models;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_STUDENT")
+public class StudentModel implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String name;
+	private Double age;
+	private String course;
+
+	public StudentModel() {
+	}
+
+	public StudentModel(Long id, String name, Double age, String course) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.course = course;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getAge() {
+		return age;
+	}
+
+	public void setAge(Double age) {
+		this.age = age;
+	}
+
+	public String getCourse() {
+		return course;
+	}
+
+	public void setCourse(String course) {
+		this.course = course;
+	}
+
+}
